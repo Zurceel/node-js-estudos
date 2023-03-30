fdcdfff2567cd875439daaf063c2ba694b0ec92b
Zurceel <gabrielcruzsil@gmail.com>
2023-03-30 09:23:24 -0300

    fazendo uma calculadoracom operadores e funcoes

--- operadores-e-funcoes/app.js
+++ operadores-e-funcoes/app.js
@@ -4,11 +4,12 @@ const readLine = require('readline').createInterface({
 });
 
 const validarNumeroInformado = (numero) => {
-    try {
-        return Number.parseFloat(numero);
-    } catch (e) {
+    const resultado = Number.parseFloat(numero);
+    if (!resultado) {
         console.log('Número informado não é válido');
     }
+    return resultado
+
 }
 
 const validarOperador = (operador) => {
@@ -25,16 +26,31 @@ const validarOperador = (operador) => {
     }
 }
 
-readLine.question('Favor informar um número:', (numero1) => {
+readLine.question('Favor informar um número: ', (numero1) => {
     const numeroValidado1 = validarNumeroInformado(numero1);
 
     if (numeroValidado1) {
-        readLine.question('Favor informar um segundo número', (numero2) => {
+        readLine.question('Favor informar um segundo número: ', (numero2) => {
             const numeroValidado2 = validarNumeroInformado(numero2);
 
             if (numeroValidado2) {
-                readLine.question('Favor informar o operador', (operador) => {
-                    
+                readLine.question('Favor informar o operador: ', (operador) => {
+                    const operadorValidado = validarOperador(operador);
+                    if (operadorValidado) {
+                        switch (operadorValidado) {
+                            case '+': console.log(`Operaodr selecionado adição, o resultado é ${numeroValidado1} + ${numeroValidado2} = ${numeroValidado1 + numeroValidado2}`);
+                                break;
+                            case '-': console.log(`Operaodr selecionado subtração, o resultado é ${numeroValidado1} - ${numeroValidado2} = ${numeroValidado1 - numeroValidado2}`);
+                                break;
+                            case '/': console.log(`Operaodr selecionado divisão, o resultado é ${numeroValidado1} / ${numeroValidado2} = ${numeroValidado1 / numeroValidado2}`);
+                                break;
+                            case '*': console.log(`Operaodr selecionado multiplicação, o resultado é ${numeroValidado1} * ${numeroValidado2} = ${numeroValidado1 * numeroValidado2}`);
+                                break;
+                            case '%': console.log(`Operaodr selecionado módulo, o resultado é ${numeroValidado1} % ${numeroValidado2} = ${numeroValidado1 % numeroValidado2}`);
+                                break;
+                            default: break;
+                        }
+                    }
                 });
             }
         });
